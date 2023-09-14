c2dff241e09d61949d6bbb1c3a37e48823ef7f3e
Billy <76769657+DavincyProject@users.noreply.github.com>
2023-09-14 14:17:43 +0700

    changes: make todoSearch responsive

--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -1,32 +1,21 @@
-// import React from 'react'
+import { useState } from "react"
 
 export default function TodoSearch() {
-  return (
-    <div>
-      <h1 className="font-bold text-2xl">TodoSearch</h1>
-      <div className="container-fluid mx-2 flex m-2 border rounded-md p-3 flex justify-around">
-        <div className="flex flex-col w-64">
-          <div className="flex gap-2">
-            <label htmlFor="search">
-              <img src="/public/search.svg" alt="search" />
-            </label>
-            <input
-              placeholder="Search Todo"
-              className="border p-1 w-full"
-              onChange=""
-            ></input>
-          </div>
+    return (
+        <div>
+            <h1 className='font-bold text-2xl'>TodoSearch</h1>
+            <div className='container-fluid mx-2 m-2 border rounded-md p-3 flex justify-around'>
+                <div className='flex flex-col w-1/2 mr-1'>
+                    <div className="flex items-center gap-2">
+                        🔎<input placeholder='Search Todo' className='border p-1 w-full' onChange={(e) => setgetItems(e.target.value)} ></input>
+                    </div>
 
-          <button className="bg-blue-500 rounded mt-2 text-white font-medium h-8">
-            Search
-          </button>
+                    <button className='bg-btn-default rounded mt-2 text-white font-medium h-8'>Search</button>
+                </div>
+                <div className='flex flex-col-reverse w-1/2'>
+                    <button className='bg-btn-default rounded mt-2 text-white font-medium w-full h-8'>Add New Task</button>
+                </div>
+            </div>
         </div>
-        <div className="flex flex-col-reverse">
-          <button className="bg-blue-500 rounded mt-2 text-white font-medium w-64 h-8">
-            Add New Task
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+    )
 }
